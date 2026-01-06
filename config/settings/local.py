05a595183bc6843bfc1660c745a97bea9077cb3e
dskim6797 <dskim6797@gmail.com>
2026-01-07 07:59:51 +0900

    🔧chore: config 파일 수정

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,12 @@
+
 from .base import *
+
 DEBUG = True
 ALLOWED_HOSTS = []
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+# Debug Toolbar Middleware
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
